0e22b9c957f31729666378d8f42a0b6cf9f370bb
Imre Kli. <imre.koli@tktk.ee>
2023-11-18 17:35:43 +0200

    add week 7 files

---
+++ ArgumentsAndParaments/Program.cs
@@ -0,0 +1,17 @@
+﻿
+Console.WriteLine("Enter a word");
+string userInput = Console.ReadLine();
+
+PrintAnyWord(userInput);  //userInpur - argument
+
+
+static void PrintAnyWord(string anyString) //anystring = parameter
+{
+    anyString = anyString.ToUpper();
+    for(int i = 0; i < 5; i++)
+    {
+        Console.WriteLine(anyString);
+    }
+
+}
+
